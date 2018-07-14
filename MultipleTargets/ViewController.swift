ebbd54789c6c26440cacdfebf63f06d9fdae2098
Shamil Yusuf <shamil707@gmail.com>
2018-07-14 18:25:41 +0800

    Multiple Target Example

--- MultipleTargets/ViewController.swift
+++ MultipleTargets/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        #if Others
+        print("MultipleTargetOthers")
+        #else
+        print("MultipleTarget")
+        #endif
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
